a88c6120e429622cb35b2fa232a9ad8a407ca3df
CNCodes <colindavid.nelson@gmail.com>
2018-09-03 15:50:57 -0700

    added files to site

---
+++ script.js
@@ -0,0 +1,80 @@
+$(window).scroll(function () {
+    var scrollLocation = $(window).scrollTop();
+    if (scrollLocation <= 30) {
+        $( '.bg-dark' ).each(function () {
+            this.style.setProperty( 'background-color', 'transparent', 'important' );
+        });
+
+        $(".navbar-dark .navbar-nav .nav-link").hover(function() {
+            $(this).css("background-color","#5e54ef");
+        }, function(){
+            $(this).css("background-color", 'transparent');
+        });
+
+        $(".navbar-dark .navbar-nav .nav-link").hover(function() {
+            $(this).css("color","white");
+        }, function(){
+            $(this).css("color", '#5e54ef');
+        });
+
+        $('.navbar').css('border-bottom-color', 'transparent');
+        $('.navbar-dark .navbar-nav .nav-link').css('background-color', 'transparent');
+        $('.navbar-dark .navbar-nav .nav-link').css('color', '#5e54ef');
+        $('.nav-link').css('border-color', 'transparent');
+
+
+
+    } else if (scrollLocation > 30) {
+        $( '.bg-dark' ).each(function () {
+            this.style.setProperty( 'background-color', '#2c2e30', 'important' );
+        });
+        $(".navbar-dark .navbar-nav .nav-link").hover(function() {
+            $(this).css("background-color","transparent")
+        }, function(){
+            $(this).css('background-color', '#5e54ef');
+        });
+
+        $(".navbar-dark .navbar-nav .nav-link").hover(function() {
+            $(this).css("color","#5e54ef");
+        }, function(){
+            $(this).css('color', 'white');
+        });
+
+
+        $('.navbar').css('border-bottom-color', '#5e54ef');
+        $('.navbar-dark .navbar-nav .nav-link').css('background-color', '#5e54ef');
+        $('.navbar-dark .navbar-nav .nav-link').css('color', 'white');
+        $('.nav-link').css('border-color', '#5e54ef');
+
+
+
+    }
+});
+
+$(document).ready(function() {
+    
+    $('.acsaa-info-button').hover(function(){
+        $('.acsaa-weblink').css('color', '#8a181a')
+    })
+    // Optimalisation: Store the references outside the event handler:
+    var $window = $(window);
+
+    function checkWidth() {
+        var windowSize = $window.width();
+        console.log(windowSize);
+        if (windowSize <= 700) {
+            console.log('success')
+            //if the window is greater than 440px wide then turn on jScrollPane..
+            $('.right-side-scroll').attr('data-aos', 'fade-up');
+            $('.left-side-scroll').attr('data-aos', 'fade-up');
+        } else {
+            $('.right-side-scroll').attr('data-aos', 'fade-right');
+            $('.left-side-scroll').attr('data-aos', 'fade-left');
+        }
+    }
+    // Execute on load
+    checkWidth();
+    // Bind event listener
+    $(window).resize(checkWidth);
+});
+
